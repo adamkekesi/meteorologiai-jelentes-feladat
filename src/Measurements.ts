9e6f6d6176450ba60decb9653860607550530cbe
SulyokDavid <sulyok.david@students.jedlik.eu>
2022-11-23 12:45:40 +0100

    fixes

--- src/Measurements.ts
+++ src/Measurements.ts
@@ -3,14 +3,14 @@ export default class Measurements {
     protected _time: string;
     protected _windDirection: string;
     protected _windSpeed: string;
-    protected _temperature: string;
+    protected _temperature: number;
 
     constructor(line: string) {
         const m: string[] = line.split(" ");
         this._city = m[0];
         this._time = m[1];
-        this._windDirection = m[3].slice(0, 2);
-        this._windSpeed = m[3].slice(3, 4);
-        this._temperature = m[4];
+        this._windDirection = m[2].slice(0, 3);
+        this._windSpeed = m[2].slice(3, 5);
+        this._temperature = +m[3];
     }
 }
